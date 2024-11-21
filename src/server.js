5ac073787692e6af5fc106a1e59b53cd5af9dff7
BoiseState Student <student@u.boisestate.edu>
2024-11-20 22:54:00 -0700

    20-Nov-2024  Final project update

--- src/server.js
+++ src/server.js
@@ -32,6 +32,51 @@ app.get('/student3', function (req, res) {
   res.render('student3')
 })
 
+
+// Begin: Added for Student3 - Christopher Smith
+
+db.run(`CREATE TABLE comments3 (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  comment TEXT NOT NULL)`)
+
+// Build the table and render the comments page
+app.get('/student3/comments', function (req, res) {
+  const local = { comments: [] }
+  db.each('SELECT id, comment FROM comments3', function (err, row) {
+      if (err) {
+      console.log(err)
+      } else {
+          local.comments.push({ id: row.id, comment: row.comment })
+      }
+  }, function (err, numrows) {
+      if (!err) {
+      res.render('student3/comments', local)
+      } else {
+      console.log(err)
+      }
+  })
+    console.log('GET called')
+
+})
+//Add a comment
+app.post('/', function (req, res) {
+    console.log('adding comments3 item')
+    const stmt = db.prepare('INSERT INTO comments3 (comment) VALUES (?)')
+    stmt.run(req.body.comments3)
+    stmt.finalize()
+})
+
+// Delete a comment
+app.post('/delete', function (req, res) {
+    console.log('deleting comments3 item')
+    //TODO you will need to delete here
+    const stmt = db.prepare('DELETE FROM comments3 where id = (?)')
+    stmt.run(req.body.id)
+    stmt.finalize()
+})
+
+// End: Added for Student3 - Christopher Smith
+
 // Start the web server
 app.listen(3000, function () {
   console.log('Listening on port 3000...')
